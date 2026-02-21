ca093b2a490331a9002dea8bcf7d01a6b853eff3
Harikrishan Sidhu <iharsidhu@gmail.com>
2026-02-21 11:12:18 -0500

    Delete learnFlaskApp directory

--- learnFlaskApp/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from learnFlaskApp import routes
--- learnFlaskApp/routes.py
+++
@@ -1,5 +0,0 @@
-from learnFlaskApp import app
-
-@app.route('/')
-def index():
-    return 'My First Flask Page'
